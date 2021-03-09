1e685371bbe5a4818ac563b96132b9b163505d59
samueltanner <samuel.m.s.tanner@gmail.com>
2021-03-09 00:25:19 -0800

    adds coin flip, playing card draw, dice roll, and lotto to gem

---
+++ lib/sam_gem/version.rb
@@ -0,0 +1,5 @@
+# frozen_string_literal: true
+
+module SamGem
+  VERSION = "0.1.0"
+end
---
+++ spec/sam_gem_spec.rb
@@ -0,0 +1,11 @@
+# frozen_string_literal: true
+
+RSpec.describe SamGem do
+  it "has a version number" do
+    expect(SamGem::VERSION).not_to be nil
+  end
+
+  it "does something useful" do
+    expect(false).to eq(true)
+  end
+end
